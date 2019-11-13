dd9de9e1a1b11df443b7e02ca85a932facd2eca6
JanuszMarek <janusz.marek92@gmail.com>
2019-11-13 20:02:51 +0100

    Create initial migration for EF Code First

--- ProductsWebAPI/Models/Product.cs
+++ ProductsWebAPI/Models/Product.cs
@@ -7,11 +7,11 @@ namespace ProductsWebAPI.Models
     {
         public Guid Id { get; set; }
 
+        [Required]
         [MaxLength(100)]
         public string Name { get; set; }
 
         [Required]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
     }
 }
