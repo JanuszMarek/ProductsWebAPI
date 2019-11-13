fc2943b9bbd043e4c000478ac01959a2efa22f1f
JanuszMarek <janusz.marek92@gmail.com>
2019-11-13 19:51:59 +0100

    Create main entity and controler.

---
+++ ProductsWebAPI/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductsWebAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+        public decimal Price { get; set; }
+    }
+}
